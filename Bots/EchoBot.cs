d5f645b6a6d0cf80329eba6d761112216ffea651
debamide <debamide@USBLRDEBAMIDE1>
2022-03-16 18:52:23 +0530

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        public QnAMaker EchoBotQnA { get; private set; }
+
+        public EchoBot(QnAMakerEndpoint endpoint)
+        {
+            // connects to QnA Maker endpoint for each turn
+            EchoBotQnA = new QnAMaker(endpoint);
+        }
+
+        private async Task AccessQnAMaker(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var results = await EchoBotQnA.GetAnswersAsync(turnContext);
+                if (results.Any())
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text("QnA Maker Returned: " + results.First().Answer), cancellationToken);
+                }
+                else
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Sorry, could not find an answer in the knowledge base."), cancellationToken);
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        //protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    var replyText = $"Echo: {turnContext.Activity.Text}";
+        //    await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+
+        //    await AccessQnAMaker(turnContext, cancellationToken);
+        //}
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Text.Contains("end") || turnContext.Activity.Text.Contains("stop"))
+            {
+                // Send End of conversation at the end.
+                var messageText = $"ending conversation from the skill...";
+                await turnContext.SendActivityAsync(MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput), cancellationToken);
+                var endOfConversation = Activity.CreateEndOfConversationActivity();
+                endOfConversation.Code = EndOfConversationCodes.CompletedSuccessfully;
+                await turnContext.SendActivityAsync(endOfConversation, cancellationToken);
+            }
+            else
+            {
+                var messageText = $"Echo: {turnContext.Activity.Text}";
+                //await turnContext.SendActivityAsync(MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text(messageText, messageText), cancellationToken);
+
+                await AccessQnAMaker(turnContext, cancellationToken);
+
+                messageText = "Say \"end\" or \"stop\" and I'll end the conversation and back to the parent.";
+                await turnContext.SendActivityAsync(MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput), cancellationToken);
+            }
+        }
+
+        protected override Task OnEndOfConversationActivityAsync(ITurnContext<IEndOfConversationActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // This will be called if the root bot is ending the conversation.  Sending additional messages should be
+            // avoided as the conversation may have been deleted.
+            // Perform cleanup of resources if needed.
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Schema;
+
+namespace EchoBot1
+{
+    public class UserProfile
+    {
+        public string Transport { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Attachment Picture { get; set; }
+
+        public string Ticket { get; set; }
+    }
+}
